603a96ae5c464851c14255aa447d933c06831fb8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-12-13 11:58:08 +0100

    dev: express working

--- back/server.js
+++ back/server.js
@@ -1,11 +1,17 @@
 console.log("About to start the server...");
 const express = require("express");
+const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
+const publicDir = "public";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
 });
+
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
